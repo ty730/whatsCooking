bbb7a99e6139d56c1deb0fa1cd6f9843a07e1d77
tawong1-1834360 <tawong1@uw.edu>
2022-07-17 17:13:48 -0700

    add bottom nav bar

---
+++ navigation/MainContainer.js
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+// Screens
+import HomeScreen from './screens/HomeScreen';
+import SearchScreen from './screens/SearchScreen';
+import CalendarScreen from './screens/CalendarScreen';
+
+//Screen names
+const homeName = "Home";
+const searchName = "Search";
+const calendarName = "Calendar";
+
+const Tab = createBottomTabNavigator();
+
+function MainContainer() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={homeName}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            let rn = route.name;
+
+            if (rn === homeName) {
+              iconName = focused ? 'home' : 'home-outline';
+
+            } else if (rn == searchName) {
+              iconName = focused ? 'list' : 'list-outline';
+
+            } else if (rn === calendarName) {
+              iconName = focused ? 'settings' : 'settings-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'grey',
+          labelStyle: { paddingBottom: 10, fontSize: 10 },
+          style: { padding: 10, height: 70}
+        }}>
+
+        <Tab.Screen name={homeName} component={HomeScreen} />
+        <Tab.Screen name={searchName} component={SearchScreen} />
+        <Tab.Screen name={calendarName} component={CalendarScreen} />
+
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default MainContainer;
